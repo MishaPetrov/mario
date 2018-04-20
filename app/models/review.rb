d4c9f33f85924858b1e585239ce4474529a57415
Misha Petrov <uxmishka@gmail.com>
2018-04-20 15:56:12 -0700

    add validations for rating and content_body

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,6 @@ class Review < ActiveRecord::Base
   validates :author, :presence => true
   validates :content_body, :presence => true
   validates :rating, :presence => true
+  validates :content_body, length: { minimum: 50, maximum: 250 }
+  validates_inclusion_of :rating, :in => 1..5
 end
