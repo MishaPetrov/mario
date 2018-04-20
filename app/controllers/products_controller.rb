4f5e96ac4bd7ee42d30c00dc0e9cf22759ceeb24
Misha Petrov <uxmishka@gmail.com>
2018-04-20 13:41:19 -0700

    addview and method for editing product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,6 +23,20 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(list_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
 private
   def list_params
     params.require(:product).permit(:name, :cost, :country)
