caa6e92c243e828116a257054c1c93d6a6fa4fce
Misha Petrov <uxmishka@gmail.com>
2018-04-20 16:18:16 -0700

    add scopes for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,16 @@ class Product < ActiveRecord::Base
   validates :name, :presence => true
   validates :cost, :presence => true
   validates :country, :presence => true
+
+  scope :most_reviews, -> {(
+    select("products.id, products.name, products.cost, products.country, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(5)
+    )}
+
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3) }
+
+  scope :made_in_usa, -> { where country: 'USA'}
 end
