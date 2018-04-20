212b9ef0ff0fe09ef5e4d1978ab16984dc61a6f9
Misha Petrov <uxmishka@gmail.com>
2018-04-20 12:56:17 -0700

    add method for new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,27 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @products = Product.find(params[:id])
+    @product = Product.find(params[:id])
     render :show
   end
+
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.new(list_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+private
+  def list_params
+    params.require(:product).permit(:name)
+  end
+
 end
