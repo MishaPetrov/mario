dc94e20fa52db0818284ff91f59ecadb732bfe76
Misha Petrov <uxmishka@gmail.com>
2018-04-20 14:04:07 -0700

    add test for editing a product

---
+++ spec/features/edit_product_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "the edit a product process" do
+  it "edits a product" do
+    visit products_path
+    click_link 'New Product'
+    fill_in 'Name', :with => 'Mango'
+    fill_in 'Cost', :with => '2'
+    fill_in 'Country', :with => 'Mexico'
+
+    click_on 'Create Product'
+    visit products_path
+    click_link 'Mango'
+    click_link 'Edit'
+    fill_in 'Name', :with => 'Pear'
+    click_on 'Update Product'
+    expect(page).to have_content 'Pear'
+  end
+end
