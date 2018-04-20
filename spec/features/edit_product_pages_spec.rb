00c9ba931f1531a0ba4167ccba279191dff6aa1a
Misha Petrov <uxmishka@gmail.com>
2018-04-20 14:06:58 -0700

    add and pass integration test for deleting a product

--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -7,13 +7,27 @@ describe "the edit a product process" do
     fill_in 'Name', :with => 'Mango'
     fill_in 'Cost', :with => '2'
     fill_in 'Country', :with => 'Mexico'
-
     click_on 'Create Product'
+
     visit products_path
     click_link 'Mango'
     click_link 'Edit'
     fill_in 'Name', :with => 'Pear'
     click_on 'Update Product'
     expect(page).to have_content 'Pear'
+  end
+
+  it "deletes a product" do
+    visit products_path
+    click_link 'New Product'
+    fill_in 'Name', :with => 'Mango'
+    fill_in 'Cost', :with => '2'
+    fill_in 'Country', :with => 'Mexico'
+    click_on 'Create Product'
+
+    visit products_path
+    click_link 'Mango'
+    click_link 'Delete'
+    expect(page).not_to have_content 'Mango'
   end
 end
